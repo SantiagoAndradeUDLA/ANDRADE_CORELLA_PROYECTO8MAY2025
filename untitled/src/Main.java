40923e6b25d113ce8a12ac8d0bbf63a67c1a6c85
seyadog <seyadog@gmail.com>
2025-05-08 16:53:02 -0500

    copia todo :)

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,13 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args){
-        String[] meses = {
-                "Enero", "Febrero", "Marzo", "Abril",
-                "Mayo", "Junio", "Julio", "Agosto",
-                "Septiembre", "Octubre", "Noviembre", "Diciembre"
-        };
+    public static void main(String[] args) {
+        String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 
         int[] lluvia = new int[12];
+        lluvia[0] = 23; lluvia[1] = 26; lluvia[2] = 20; lluvia[3] = 22; lluvia[4] = 25;
+        lluvia[5] = 22; lluvia[6] = 26; lluvia[7] = 25; lluvia[8] = 22; lluvia[9] = 24;
+        lluvia[10] = 26; lluvia[11] = 23;
+
+        for(int i = 0; i < lluvia.length; i++) {
+            System.out.println("La lluvia en " + meses[i] + " es: " + lluvia[i] + " l/m²");
+        }
     }
 }
