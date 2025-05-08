d047539fed6de8339d7a4f1e2eee4867a42ca4f0
seyadog <seyadog@gmail.com>
2025-05-08 17:05:23 -0500

    Acumulado lluvia y promedio

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,3 +1,5 @@
+import java.security.Provider;
+
 public class Main {
     public static void main(String[] args) {
         String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
@@ -8,12 +10,17 @@ public class Main {
         lluvia[5] = 22; lluvia[6] = 26; lluvia[7] = 25; lluvia[8] = 22; lluvia[9] = 24;
         lluvia[10] = 26; lluvia[11] = 23;
 
+        double prom = 0;
         int total = 0;
         for(int i = 0; i < lluvia.length; i++) {
             System.out.println("La lluvia en " + meses[i] + " es: " + lluvia[i] + " l/m²");
 
             total +=  lluvia[i];
-            System.out.println("total lluvia iteracion = " + total);
+
+            prom = total/12;
+
+            System.out.println("total lluvia acumulada = " + total);
+            System.out.println(("el promedio de lluvia mansual = " + prom ));
 
         }
 
