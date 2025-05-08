e04f21f0c5cb9d71ddc53a42b6dcd5464ce245f9
seyadog <seyadog@gmail.com>
2025-05-08 17:00:52 -0500

    Acumulado lluvia

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -8,8 +8,14 @@ public class Main {
         lluvia[5] = 22; lluvia[6] = 26; lluvia[7] = 25; lluvia[8] = 22; lluvia[9] = 24;
         lluvia[10] = 26; lluvia[11] = 23;
 
+        int total = 0;
         for(int i = 0; i < lluvia.length; i++) {
             System.out.println("La lluvia en " + meses[i] + " es: " + lluvia[i] + " l/m²");
+
+            total +=  lluvia[i];
+            System.out.println("total lluvia iteracion = " + total);
+
         }
+
     }
 }
